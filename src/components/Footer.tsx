53ff612daea923de220741960e53d708a46af340
vbzt <vitorcastrobuzato@gmail.com>
2025-02-12 16:22:02 -0300

    deploy

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,7 @@ import styles from './Footer.module.css'
 const Footer = () => {
   return (
     <footer className = {styles.footer}>
-       <p>Made with 💗 by <a className = {styles.github} target="_blank" href="https://github.com/vbzt/Any2Any">vbzt</a>.</p>
-       
-      <p>Any2<span className="colored">Any</span>&copy; 2025</p>
+       <p>Made with 💙 by <a className = {styles.github} target="_blank" href="https://github.com/vbzt/Any2Any">vbzt</a> &copy;.</p>
     </footer>
   )
 }
